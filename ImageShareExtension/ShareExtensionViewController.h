8d20d753e7f05cf95b6dd8f553d0842b04fd055e
Hzy <794918724@qq.com>
2024-03-19 19:12:14 +0800

    update 3.19

---
+++ ImageShareExtension/ShareExtensionViewController.h
@@ -0,0 +1,16 @@
+//
+//  ShareExtensionViewController.h
+//  ImageShareExtension
+//
+//  Created by hzy on 2024-03-18.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ShareExtensionViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
